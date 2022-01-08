4cbc1e99b22ced4a232808fb9e6f89fad39c51f8
Viktor Habchak <vikdotdev@gmail.com>
2022-01-08 23:01:53 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+
+# export ANSIBLE_HOST_KEY_CHECKING=False
+# use this script do fetch all ansible-galaxy deps
+# ansible-galaxy collection install community.general
+# ansible-galaxy install rvm.ruby
