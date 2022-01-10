f4254932c6fcaf51718ba2bc109bcef47e7f3524
Viktor Habchak <vikdotdev@gmail.com>
2022-01-10 18:54:39 +0200

    remove install script; it's replaced by readme instructions

--- install.sh
+++
@@ -1,5 +0,0 @@
-
-# export ANSIBLE_HOST_KEY_CHECKING=False
-# use this script do fetch all ansible-galaxy deps
-# ansible-galaxy collection install community.general
-# ansible-galaxy install rvm.ruby
